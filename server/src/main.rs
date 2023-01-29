d45b7b7a1e8261817650f2a0138dde3ad039bd67
plash_har <plashar@outlook.fr>
2023-01-29 16:30:42 +0100

    Renamed web_types to net_types

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,10 @@
+use std::time::Duration;
+
 use game_logic::World;
+
+mod interface;
+
+const WORLD_UPD_RATE: Duration = Duration::from_millis(1000 / 60);  // 60 times per second
 
 fn main() {
     let mut world = World::new(3, 3);
